818aa9acae41e891701fa9c93f6d517867a5624e
Sahoo <pallabi.sahoo@accenture.com>
2022-07-29 13:09:50 +0530

    config created

---
+++ housing/config/configuration.py
@@ -0,0 +1,24 @@
+classs configuration:
+    def__init__(self) -> None:
+        pass
+
+    def get_data_ingestion_config(self) ->DataIngestionConfig:
+        pass
+
+    def get_data_validation_config(self):
+        pass
+    
+    def get_data_transformation_config(self):
+        pass
+    
+    def get_model_trainer_config(self):
+        pass
+
+    def get_model_evaluation_config(self):
+        pass
+    
+    def get_model_pusher_config(self):
+        pass
+
+    def get_training_pipeline_config(self):
+        pass
